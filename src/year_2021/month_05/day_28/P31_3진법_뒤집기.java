d4026a26bd6fbef5760e41ac2aa7c3a0ad85d49e
JungSangHyeon <solea1204@naver.com>
2021-05-28 14:23:00 +0900

    31

---
+++ src/year_2021/month_05/day_28/P31_3진법_뒤집기.java
@@ -0,0 +1,28 @@
+package year_2021.month_05.day_28;
+
+public class P31_3진법_뒤집기 {
+
+	/**
+	 * 시험이라 바쁘지렁
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println(solution(45));
+		System.out.println(solution(125));
+	}
+
+	public static int solution(int n) {
+		String temp = "";
+		while(n/3>0) {
+			temp+=n%3;
+			n/=3;
+		}
+		temp+=n%3;
+		
+		int answer=0;
+		for(int i=0; i<temp.length(); i++) {
+			answer+= (temp.charAt(i)-'0')*Math.pow(3, temp.length()-i-1);
+		}
+		return answer;
+	}
+}
