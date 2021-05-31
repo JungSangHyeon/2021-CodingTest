814a865ea46bbeff5acba48da3d7a5c07ad0bd7f
JungSangHyeon <solea1204@naver.com>
2021-05-31 15:33:42 +0900

    34

---
+++ src/year_2021/month_05/day_31/P34_문자열_내_마음대로_정렬하기.java
@@ -0,0 +1,15 @@
+package year_2021.month_05.day_31;
+
+import java.util.Arrays;
+
+public class P34_문자열_내_마음대로_정렬하기 {
+
+	public static void main(String[] args) {
+	}
+
+	public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (s1,s2)-> (s1.charAt(n)-s2.charAt(n)==0)? s1.compareTo(s2):s1.charAt(n)-s2.charAt(n));
+        return strings;
+    }
+	
+}
