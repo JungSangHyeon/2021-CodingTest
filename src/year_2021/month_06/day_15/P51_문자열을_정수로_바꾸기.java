c4d0c257e57f3364c82c3a40bedb872c6ee30dc1
JungSangHyeon <solea1204@naver.com>
2021-06-15 11:55:03 +0900

    51

---
+++ src/year_2021/month_06/day_15/P51_문자열을_정수로_바꾸기.java
@@ -0,0 +1,12 @@
+package year_2021.month_06.day_15;
+
+public class P51_문자열을_정수로_바꾸기 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
