b56989b31c8ac09a2c08d68bc17207e70a7d041e
JungSangHyeon <solea1204@naver.com>
2021-06-28 09:01:59 +0900

    65

---
+++ src/year_2021/month_06/day_28/P65_평균_구하기.java
@@ -0,0 +1,17 @@
+package year_2021.month_06.day_28;
+
+import java.util.Arrays;
+
+public class P65_Æò±Õ_±¸ÇÏ±â {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double solution(int[] arr) {
+        return Arrays.stream(arr).average().getAsDouble();
+	}
+        
+        
+}
