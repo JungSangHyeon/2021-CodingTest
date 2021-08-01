8f77f64c12b965f810af9dd68584a5a78f60bd25
JungSangHyeon <solea1204@naver.com>
2021-08-01 10:37:22 +0900

    111

---
+++ src/year_2021/month_08/P111_다음_큰_숫자.java
@@ -0,0 +1,24 @@
+package year_2021.month_08;
+
+public class P111_다음_큰_숫자 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(78));
+	}
+
+	public static int solution(int n) {
+		int targetOneCount = getOneCountOfBinary(n);
+        int next = n+1;
+        while(getOneCountOfBinary(next) != targetOneCount) next++;
+        return next;
+    }
+	
+	public static int getOneCountOfBinary(int n) {
+		String binaryN = Integer.toBinaryString(n);
+		int oneCount = 0;
+		for(char c : binaryN.toCharArray()) {
+			if(c=='1') oneCount++;
+		}
+		return oneCount;
+	}
+}
