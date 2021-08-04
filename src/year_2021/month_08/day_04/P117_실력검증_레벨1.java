436bc7988e685710d7c5ad8313a98bfb753c8191
JungSangHyeon <solea1204@naver.com>
2021-08-04 13:34:36 +0900

    117

---
+++ src/year_2021/month_08/day_04/P117_실력검증_레벨1.java
@@ -0,0 +1,15 @@
+package year_2021.month_08.day_04;
+
+public class P117_실력검증_레벨1 {
+
+	public static void main(String[] args) {
+
+		System.out.println(solution(12));
+	}
+	
+	public static int solution(int n) {
+		// 코드는 프로그래머스에서 확인
+		return 1;
+    }
+
+}
