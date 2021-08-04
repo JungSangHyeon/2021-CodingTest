9697642e5ef94f3ce43b1978b6cb0e770af86b37
JungSangHyeon <solea1204@naver.com>
2021-08-04 13:34:40 +0900

    118

---
+++ src/year_2021/month_08/day_04/P118_실력검증_레벨1.java
@@ -0,0 +1,14 @@
+package year_2021.month_08.day_04;
+
+public class P118_실력검증_레벨1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int[] solution(int[] arr, int divisor) {
+		// 코드는 프로그래머스에서 확인
+		return new int[] { -1 };
+	}
+}
