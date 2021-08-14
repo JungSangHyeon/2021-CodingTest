71415c6ac23c885fd4536c9cea961cab831eb76a
JungSangHyeon <solea1204@naver.com>
2021-08-14 14:00:32 +0900

    133

--- src/year_2021/month_08/day_14/P133_N진수_게임.java
+++ src/year_2021/month_08/day_14/P133_N진수_게임.java
@@ -2,7 +2,7 @@ package year_2021.month_08.day_14;
 
 public class P133_N진수_게임 {
 
-	public static void main(String[] args) {   
+	public static void main(String[] args) {    
 		System.out.println(solution(2,4,2,1));
 		System.out.println(solution(16,16,2,1));
 		System.out.println(solution(16,16,2,2));
