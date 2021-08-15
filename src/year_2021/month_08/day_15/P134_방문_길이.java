7e1969ce64bccc695eae75deea078e7a539647d3
JungSangHyeon <dev.jusa@gmail.com>
2021-08-15 12:23:05 +0900

    134

--- src/year_2021/month_08/day_15/P134_방문_길이.java
+++ src/year_2021/month_08/day_15/P134_방문_길이.java
@@ -1,10 +1,34 @@
 package year_2021.month_08.day_15;
 
+import java.util.*;
+
 public class P134_방문_길이 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(solution("LULLLLLLU"));
 	}
 
+	public static int solution(String dirs) {
+		HashSet<String> move = new HashSet<>();
+		int x=0, y=0;
+
+		for(Character c : dirs.toCharArray()) {
+			int dx=x, dy=y;
+			switch (c) {
+				case 'U' : dy = y+1; break;
+				case 'D' : dy = y-1; break;
+				case 'R' : dx = x+1; break;
+				case 'L' : dx = x-1; break;
+			}
+			if(Math.abs(dx)<=5 && Math.abs(dy)<=5) {
+				move.add(""+x+y+dx+dy);
+				move.add(""+dx+dy+x+y);
+				System.out.println("("+x+", "+y+") -> ("+dx+", "+dy+")");
+				System.out.println();
+				x=dx;
+				y=dy;
+			}
+		}
+		return move.size()/2;
+	}
 }
