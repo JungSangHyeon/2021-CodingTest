f31851f900ae4b4e2ac9a14c0cc0fc9c35d5e164
JungSangHyeon <dev.jusa@gmail.com>
2021-08-29 20:20:26 +0900

    148

---
+++ src/year_2021/month_08/day_29/P148_BOJ1912_연속합.java
@@ -0,0 +1,25 @@
+package year_2021.month_08.day_29;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P148_BOJ1912_¿¬¼ÓÇÕ {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] numbers = new int[n];
+        for(int i=0; i<n; i++) numbers[i] = Integer.parseInt(st.nextToken());
+
+        int[] memory = new int[n];
+        memory[0] = numbers[0];
+        int max = memory[0];
+        for(int i=1; i<n; i++){
+            memory[i] = Math.max(memory[i-1]+numbers[i], numbers[i]);
+            if(max < memory[i]) max=memory[i];
+        }
+        System.out.println(max);
+    }
+}
