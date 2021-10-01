41fc96d53b370ad523fdd4af91f8ffb1de95005c
JungSangHyeon <dev.jusa@gmail.com>
2021-10-01 15:55:49 +0900

    error

--- src/tool/ArrayStringMaker.java
+++ src/tool/ArrayStringMaker.java
@@ -4,7 +4,7 @@ public class ArrayStringMaker {
 
 	public static void main(String[] args) {
 
-		String target = "[[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]]";
+		String target = "[[\"100\",\"ryan\",\"music\",\"2\"],[\"200\",\"apeach\",\"math\",\"2\"],[\"300\",\"tube\",\"computer\",\"3\"],[\"400\",\"con\",\"computer\",\"4\"],[\"500\",\"muzi\",\"music\",\"3\"],[\"600\",\"apeach\",\"music\",\"2\"]]";
 		System.out.println(target.replace("[", "{").replace("]", "}"));
 	}
 
