5685c13bbe2bf5d6dc3651c1f7650d2e9b3a90b6
JungSangHyeon <dev.jusa@gmail.com>
2021-10-11 20:16:06 +0900

    191

---
+++ src/year_2021/month_10/day_11/P191_BOJ10718_WeLoveKriii.java
@@ -0,0 +1,9 @@
+package year_2021.month_10.day_11;
+
+public class P191_BOJ10718_WeLoveKriii {
+
+    public static void main(String[] args){
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
