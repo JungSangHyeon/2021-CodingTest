b95ecb99adb01175bf7e6db76974e65dec7652d5
JungSangHyeon <dev.jusa@gmail.com>
2021-10-15 17:18:02 +0900

    195

---
+++ src/year_2021/month_10/day_15/P195_BOJ10171_고양이.java
@@ -0,0 +1,11 @@
+package year_2021.month_10.day_15;
+
+public class P195_BOJ10171_°í¾çÀÌ {
+
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
