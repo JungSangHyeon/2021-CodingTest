555ae77f0c45bdce2cd3d023cc5e8b8ae3700e3a
JungSangHyeon <dev.jusa@gmail.com>
2021-10-16 15:14:39 +0900

    196

---
+++ src/year_2021/month_10/day_16/P196_BOJ10172_개.java
@@ -0,0 +1,13 @@
+package year_2021.month_10.day_16;
+
+public class P196_BOJ10172_°³ {
+
+    public static void main(String[] args){
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
