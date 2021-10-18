baccc0c81579703afd5c3539c299ab6eb4c7a074
JungSangHyeon <dev.jusa@gmail.com>
2021-10-18 10:50:25 +0900

    197

---
+++ src/year_2021/month_10/day_17/P197_BOJ1000_AB.java
@@ -0,0 +1,16 @@
+package year_2021.month_10.day_17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P197_BOJ1000_AB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println(A + B);
+    }
+}
