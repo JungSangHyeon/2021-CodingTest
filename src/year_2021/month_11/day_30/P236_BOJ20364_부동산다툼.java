8634b52c587c92e569b935625e43976077eedb96
JungSangHyeon <dev.jusa@gmail.com>
2021-11-30 10:10:27 +0900

    236

--- src/year_2021/month_11/day_30/P236_BOJ20364_부동산다툼.java
+++ src/year_2021/month_11/day_30/P236_BOJ20364_부동산다툼.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class P236_BOJ20364_부동산다툼 {
+public class P236_BOJ20364_부동산다툼 { // 커밋 잘못했다...
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
